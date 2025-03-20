6824cb3519bdb7e4c85df0db0e627ed6990ba71b
Cyanmin <tomohiro20030220@gmail.com>
2025-03-20 14:32:24 +0900

    Initial commit 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
+import { defineConfig } from 'vite'  // 追加
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({  // 変更
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  server: {
+    host: "0.0.0.0",
+    port: 3000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
